331ecd198f657e0991669dc3f49f7c6faf0edf54
chneau <charles63500@gmail.com>
2021-06-30 00:06:11 +0100

    styled

--- Todo.Web/ClientApp/src/pages/HomePage.tsx
+++ Todo.Web/ClientApp/src/pages/HomePage.tsx
@@ -1,17 +1,50 @@
 import React from "react";
-import "./HomePage.css";
+import styled from "styled-components";
 import logo from "./logo.svg";
 
+const Logo = styled.img`
+  height: 40vmin;
+  pointer-events: none;
+
+  @media (prefers-reduced-motion: no-preference) {
+    animation: App-logo-spin infinite 20s linear;
+  }
+
+  @keyframes App-logo-spin {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+`;
+
+const Header = styled.header`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
+
+const HomeLink = styled.a`
+  color: #61dafb;
+`;
+
 export const HomePage = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
+  <div>
+    <Header>
+      <Logo src={logo} alt="logo" />
       <p>
         Edit <code>src/App.tsx</code> and save to reload.
       </p>
-      <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+      <HomeLink href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
         Learn React
-      </a>
-    </header>
+      </HomeLink>
+    </Header>
   </div>
 );
