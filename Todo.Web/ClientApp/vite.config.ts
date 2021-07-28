60ecfbf48725fad6a402255cbfdb710f2a9d47f7
chneau <charles63500@gmail.com>
2021-07-28 16:50:57 +0100

    fix

--- Todo.Web/ClientApp/vite.config.ts
+++ Todo.Web/ClientApp/vite.config.ts
@@ -4,10 +4,4 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh()],
-  base: "/dist/",
-  server: {
-    hmr: {
-      protocol: "ws",
-    },
-  },
 });
