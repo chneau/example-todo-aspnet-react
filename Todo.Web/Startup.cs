2123984d252823dd5836fffe37a74722ba70f65c
chneau <charles63500@gmail.com>
2021-07-28 17:16:36 +0100

    working through http

--- Todo.Web/Startup.cs
+++ Todo.Web/Startup.cs
@@ -1,13 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Todo.Web.Data;
-using Todo.Web.GraphQL;
 using Todo.Web.GraphQL.Authors;
 using Todo.Web.GraphQL.Books;
 using Todo.Web.GraphQL.GraphQL.Extensions;
@@ -68,18 +66,14 @@ namespace Todo.Web
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            if (env.IsDevelopment()) { app.UseDeveloperExceptionPage(); }
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
+                app.UseHttpsRedirection();
             }
 
-            app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseSpaStaticFiles();
 
@@ -92,15 +86,7 @@ namespace Todo.Web
                 endpoints.MapControllerRoute(name: "default", pattern: "{controller}/{action=Index}/{id?}");
             });
 
-            app.UseSpa(spa =>
-            {
-                spa.Options.SourcePath = "ClientApp";
-
-                if (env.IsDevelopment())
-                {
-                    spa.UseReactDevelopmentServer(npmScript: "start");
-                }
-            });
+            if (env.IsDevelopment()) { app.UseSpa(spa => spa.UseProxyToSpaDevelopmentServer("http://localhost:3000")); }
         }
     }
 }
